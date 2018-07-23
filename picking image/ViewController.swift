758766804ebb5c68a52e6a8b61122714ef66cdcf
TahaMagdy <tahamagdy314@gmail.com>
2018-07-23 20:26:26 +0200

    moveing keyboard done

--- picking image/ViewController.swift
+++ picking image/ViewController.swift
@@ -10,23 +10,55 @@ import UIKit
 
 class ViewController: UIViewController,
     UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate {
+    UINavigationControllerDelegate,
+    UITextFieldDelegate {
 
     
     
     
     
     @IBOutlet weak var imageViewObj: UIImageView!
+    @IBOutlet weak var textField: UITextField!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
     }
 
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Subscribe to Keyboard Notification willShow.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(moveTheKeyboardAway(_:)),
+                                               name: .UIKeyboardWillShow,
+                                               object: nil)
+        
+        // Subscribe to keyboard willHide Notification.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(getTheViewBack(_:)),
+                                               name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: .UIKeyboardWillShow,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: .UIKeyboardWillHide,
+                                                  object: nil)
+        
+    }
 
     
-    
+
     // Pick and image
     @IBAction func pickImage(_ sender: Any) {
         
@@ -45,9 +77,9 @@ class ViewController: UIViewController,
 
     
     
+    // Called when an image is selected
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         print("Image is selected")
         
         
@@ -58,19 +90,51 @@ class ViewController: UIViewController,
         
         // You can add the line belw to viewWillAppear().
         // cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-
     }
     
     
     
+    
+    
+    // Called when cancel button is selected
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         print("Cancel is pressed")
         picker.dismiss(animated: true, completion: nil)
-        
     }
     
     
+    /*
+     * notification carries information in the notification.userInfo dictionary.
+    */
+    @objc func moveTheKeyboardAway(_ notification: Notification) {
+        
+        let userInfo = notification.userInfo
+        let keyboadSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        view.frame.origin.y -= keyboadSize.cgRectValue.height
+    } // end moveTheKeyboardAway
     
+    
+    
+    
+    // Called when the Notification .UIKeyboardWillHide happens
+    // We need to subscribe first.
+    @objc func getTheViewBack(_ notification: Notification) {
+        
+        view.frame.origin.y = 0
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.text = ""
+        return true
+    }
+
     
     
 } // end ViewController
